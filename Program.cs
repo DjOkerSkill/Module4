8ac3488cd207c239afc3d9f366fba43f378f9706
Роман <ram23@mail.ru>
2022-04-09 19:39:36 +0300

    Итоговое задание по Модулю 4

--- Program.cs
+++ Program.cs
@@ -6,31 +6,53 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-            int change;
+            (string nameUser, string lastNameUser, string login, int loginLength, bool hasPet, int ageUser, string[] colorUser ) User;
 
-            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            int countUser=0;
+
+            do
             {
-                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                Console.WriteLine("Введите имя");
+
+                User.nameUser = Console.ReadLine();
+
+                Console.WriteLine("Введите фамилию");
+
+                User.lastNameUser = Console.ReadLine();
+
+                Console.WriteLine("Введите логин");
+
+                User.login = Console.ReadLine();
+
+                User.loginLength = User.login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+
+                string Pet = Console.ReadLine();
+
+                if (Pet == "Да" || Pet == "да")
                 {
-                    for (int k = 1+j; k < arr.GetUpperBound(1); k++)
-                    {
-                        if (arr[i,j]>arr[i,k])
-                        {
-                            change=arr[i,j];
-                            arr[i,j]=arr[i,k];
-                            arr[i,k]=change;
-
-                        }
-                        
-                    }
-
-                    Console.Write(arr[i, j]);
+                    User.hasPet = true;
                 }
-                Console.WriteLine();
+                else
+                    User.hasPet = false;
+
+                Console.WriteLine("Введите возраст пользователя");
+
+                User.ageUser = int.Parse(Console.ReadLine());
+
+
+                User.colorUser = new string[3];
+
+                Console.WriteLine("Введите три любимых цвета пользователя");
+
+                for (int i = 0; i < User.colorUser.Length; i++)
+                {
+                    User.colorUser[i] = Console.ReadLine();
+                }
+                ++countUser;
             }
-            
-              
+            while (countUser<3);
         }
         
     }
