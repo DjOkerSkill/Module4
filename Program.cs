c2cb897ad994b387b6eddf12641169a016f7a254
Роман <ram23@mail.ru>
2022-04-09 12:42:59 +0300

    Задание 4.1.17

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Напишите свой любимый цвет на английском языке с маленькой буквы");
+
+            var color = Console.ReadLine();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red");
+            }
+            else if (color == "green") 
+            { 
+                Console.BackgroundColor= ConsoleColor.Green;
+                Console.ForegroundColor= ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green");
+            }
+            else
+            {
+                Console.BackgroundColor=ConsoleColor.Cyan;
+                Console.ForegroundColor= ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan");
+            }
         }
     }
 }
