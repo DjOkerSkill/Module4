2fc77de52fbf1336a751da6a8e3c925ed8342ada
Роман <ram23@mail.ru>
2022-04-09 17:25:37 +0300

    Task 4.3.17

--- Program.cs
+++ Program.cs
@@ -7,20 +7,30 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-            int count = 0;
+            int change;
 
-            for (int i = 0; i < arr.GetUpperBound(0)+1; i++)
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
             {
-                for (int j = 0; j < arr.GetUpperBound(1) + 1; j++)
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
                 {
-                    if (arr[i,j] > 0)
+                    for (int k = 1+j; k < arr.GetUpperBound(1); k++)
                     {
-                    count++;
+                        if (arr[i,j]>arr[i,k])
+                        {
+                            change=arr[i,j];
+                            arr[i,j]=arr[i,k];
+                            arr[i,k]=change;
+
+                        }
+                        
                     }
+
+                    Console.Write(arr[i, j]);
                 }
+                Console.WriteLine();
             }
             
-              Console.WriteLine(count);
+              
         }
         
     }
