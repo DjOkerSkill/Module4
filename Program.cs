437357c85c188d70eae5c1e54aec5f4d0b259044
Роман <ram23@mail.ru>
2022-04-09 12:57:03 +0300

    Task 4.1.18

--- Program.cs
+++ Program.cs
@@ -10,27 +10,38 @@ namespace ConsoleApp2
 
             var color = Console.ReadLine();
 
-            if (color == "red")
+            switch (color)
             {
+                case "red":
+                
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.ForegroundColor = ConsoleColor.Black;
-
                 Console.WriteLine("Your color is red");
-            }
-            else if (color == "green") 
-            { 
-                Console.BackgroundColor= ConsoleColor.Green;
-                Console.ForegroundColor= ConsoleColor.Black;
+                break;
 
+                case "green":
+                
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
                 Console.WriteLine("Your color is green");
-            }
-            else
-            {
-                Console.BackgroundColor=ConsoleColor.Cyan;
-                Console.ForegroundColor= ConsoleColor.Black;
+                break;
 
-                Console.WriteLine("Your color is cyan");
+                case "cyan":
+
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("Your color is cyan");
+                    break;
+
+                default:
+                
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your color is Yellow");
+                break ;
             }
         }
+        
+        
     }
 }
